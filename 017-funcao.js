69c5efc8fc1f0f86be82344e6b60fc143d6f97b2
MaxsuelR10 <maxsuelrodriguessantos15@gmail.com>
2025-09-15 21:27:30 -0300

    aula 15/09

---
+++ 017-funcao.js
@@ -0,0 +1,24 @@
+let entrada = require('prompt-sync')();
+// exemplo de função que não retorna valor
+function olaUsuario(pNome) {
+    console.log(`Olá ${pNome}, seja bem vindo(a)!`);
+}
+
+function loginUsuario(pUsuario, pSenha) {
+    if (pUsuario === 'admin' && pSenha === '1234'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const usuario = entrada('Digite seu usuário: ');
+const senha = entrada('Digite sua senha: ');
+
+if (loginUsuario(usuario, senha)){
+    let nome = entrada('Digite seu nome: ');
+    olaUsuario(nome);
+}
+else {
+    console.log('Usuário ou senha inválido!');
+}
