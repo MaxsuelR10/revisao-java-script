97e80bac2ba15f65c0f0f6687077f910a7987bc8
MaxsuelR10 <maxsuelrodriguessantos15@gmail.com>
2025-09-23 19:23:45 -0300

    atualização 23/09

--- 018-sistemalogin.js
+++ 018-sistemalogin.js
@@ -1,30 +1,33 @@
 let entrada = require('prompt-sync')();
 
 let usuarios = [
-    {usuario: 'admin', senha: '1234', nome: 'Administrador'},
-    {usuario: 'jose', senha: 'abcd', nome: 'José da Silva'},
-    {usuario: 'maria', senha: 'xyz', nome: 'Maria de Souza'},
-]
+    { usuario: 'admin', senha: '1234', nome: 'Administrador' },
+    { usuario: 'jose', senha: 'abcd', nome: 'José da Silva' },
+    { usuario: 'maria', senha: 'xyz', nome: 'Maria de Souza' }
+];
+
 // exemplo de função que não retorna valor
 function olaUsuario(pNome) {
     console.log(`Olá ${pNome}, seja bem vindo(a)!`);
 }
 
+// exemplo de função que retorna valor
 function loginUsuario(pUsuario, pSenha) {
-   let logado = false
-   let i = 0
-   for (i; i < usuarios.length; i++) {
-    if (pUsuario == usuarios[i].usuario && pSenha == usuarios[i].senha) {
-        logado = true;
-        break;
+    let logado = false
+    let i = 0
+    for (i ; i < usuarios.length; i++) {
+        if (pUsuario == usuarios[i].usuario && pSenha == usuarios[i].senha) {
+            logado = true;
+            break;
+        }
     }
-   }
-   if(logado) {
-    return { sucesso: true, txtRetorno: usuarios[i].nome};
-   } else {
-    return { sucesso: false, txtRetorno: 'Usuário ou senha invalida!' };
-   }
+    if (logado) {
+        return { sucesso: true, txtRetorno: usuarios[i].nome };
+    } else {
+        return { sucesso: false, txtRetorno: 'Usuário ou senha inválidos!' };
+    }
 }
+
 const usuario = entrada('Digite seu usuário: ');
 const senha = entrada('Digite sua senha: ');
 
@@ -33,6 +36,9 @@ const retorno = loginUsuario(usuario, senha);
 if (retorno.sucesso) {
     let nome = retorno.txtRetorno;
     olaUsuario(nome);
-} else{
+} else {
     console.log(retorno.txtRetorno);
 }
+
+
+
